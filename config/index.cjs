9ddf5dca7e6303c20a73aee133fbb9addf44f2e3
pay888 <59396165+pay888@users.noreply.github.com>
2022-11-13 02:12:21 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oTzPL6QiP1c1cq0vL0HM_AHItlcI',
+      id: 'oTzPL6RkYEGXjmArLmrr69C711k0',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'uZ8EAapnR1Fj4g32o2SqlUMLOcVKTlOS9kW62VCuEAg',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
